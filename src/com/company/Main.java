604943054ab4dd728185afd883fdc5f460895266
Dewy Yuliana <dewy.yuliana@1rstwap.com>
2019-08-12 17:05:05 +0700

    Adding Challenge

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int s = a;
+
+        }
+        in.close();
+    }
+}
